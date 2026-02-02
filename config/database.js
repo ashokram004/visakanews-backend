5470dad1ebbcc7d55cbecb1c45f8a6621aeda85e
Ashok Ram <ak9000812219@gmail.com>
2026-02-02 16:14:29 +0530

    fix

--- config/database.js
+++ config/database.js
@@ -30,22 +30,28 @@ module.exports = ({ env }) => {
     },
 
     postgres: {
-	  connection: {
-		host: env('DATABASE_HOST'),
-		port: env.int('DATABASE_PORT', 5432),
-		database: env('DATABASE_NAME'),
-		user: env('DATABASE_USERNAME'),
-		password: env('DATABASE_PASSWORD'),
-		ssl: {
-		  rejectUnauthorized: env.bool(
-			'DATABASE_SSL_REJECT_UNAUTHORIZED',
-			false
-		  ),
-		},
-		schema: env('DATABASE_SCHEMA', 'public'),
-	  },
-	  pool: { min: 2, max: 10 },
-	},
+      connection: {
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME'),
+        user: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: env.bool('DATABASE_SSL', false)
+          ? {
+              rejectUnauthorized: env.bool(
+                'DATABASE_SSL_REJECT_UNAUTHORIZED',
+                false
+              ),
+            }
+          : false,
+        schema: env('DATABASE_SCHEMA', 'public'),
+      },
+      pool: {
+        min: env.int('DATABASE_POOL_MIN', 2),
+        max: env.int('DATABASE_POOL_MAX', 10),
+      },
+    },
+    
 
     sqlite: {
       connection: {
